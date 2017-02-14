25e5e07cbcd1c7eda13a9732eaf39b954c50ddb5
Ekaterina Avdeeva <ekaterina.avdeeva@cern.ch>
2017-02-14 14:57:59 -0600

    script to copy figs and text from AN area

---
+++ nuthesis/doCopyANnote.sh
@@ -0,0 +1,39 @@
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v11 /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/
+	
+
+d1=/home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk
+d2=examples
+
+cp $d1/Samples.tex $d2/AN_Samples.tex
+cp $d1/ObjectID.tex $d2/AN_ObjectID.tex
+cp $d1/BackgroundSubtraction.tex $d2/AN_BackgroundSubtraction.tex
+cp $d1/Unfolding.tex $d2/AN_Unfolding.tex
+cp $d1/AccXEff.tex $d2/AN_AccXEff.tex
+cp $d1/Systematics.tex $d2/AN_Systematics.tex
+cp $d1/CrossSection.tex $d2/AN_CrossSection.tex
+
+cp $d1/Appendix_ZgFSRandISRplots.tex $d2/
+cp $d1/Appendix_CompareTemplatesPlots.tex $d2/
+cp $d1/Appendix_TemplateFitPlots.tex $d2/
+cp $d1/Appendix_TemplateFitPlotsMCclosure.tex $d2/
+cp $d1/Appendix_TemplateFitPlotsMCclosureWjestPlusWg.tex $d2/
+cp $d1/Appendix_Code.tex $d2/
+cp $d1/Appendix_ZgCheck.tex $d2/
+cp $d1/Appendix_TemplateFitPlots_ZGamma.tex $d2/
+cp $d1/Appendix_TemplateFitPlotsMCclosure.tex $d2/
+cp $d1/Appendix_TemplateFitPlotsMCclosureWjestPlusWg.tex $d2/
+cp $d1/Appendix_Code.tex $d2/
+cp $d1/Appendix_ZgCheck.tex $d2/
+cp $d1/Appendix_TemplateFitPlots_ZGamma.tex $d2/
+cp $d1/Appendix_TemplateFitPlotsMCclosure_ZGamma.tex $d2/
+cp $d1/Appendix_WjetsCheck.tex $d2/
+cp $d1/Appendix_MCclosureCheck.tex $d2/
+cp $d1/Appendix_EtogFitPlots.tex $d2/
+cp $d1/Appendix_EtogFitPlotsNoWMtCut.tex $d2/
+cp $d1/Appendix_EtogTables.tex $d2/
+cp $d1/Appendix_CorrelationMatrices.tex $d2/
+cp $d1/Appendix_Mpholep1DatavsMCPlots.tex $d2/
+
+
+
+ 
