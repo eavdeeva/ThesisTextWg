0bf0495952a53f870daee6535af2dc19b41be433
Ekaterina Avdeeva <ekaterina.avdeeva@cern.ch>
2017-02-17 16:01:38 -0600

    Appendixes prepared (just very first drafts)

--- nuthesis/doCopyANnote.sh
+++ nuthesis/doCopyANnote.sh
@@ -1,38 +1,53 @@
 #cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v11 /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/
-	
+
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/MUON_ZGamma/PrepareYields/*FSR*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/MUON_ZGamma/PrepareYields/	
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/MUON_ZGamma/PrepareYields/*Mpholeplep*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/MUON_ZGamma/PrepareYields/
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/MUON_ZGamma/PrepareYields/*Mleplep*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/MUON_ZGamma/PrepareYields/
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/MUON_ZGamma/PrepareYields/*PhoDeltaR*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/MUON_ZGamma/PrepareYields/
+
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v5/ChannelsMERGED_ZGamma/QuickChecks/*Templates*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/ChannelsMERGED_ZGamma/QuickChecks/
+
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v5/ChannelsMERGED_WGamma/QuickChecks/*Templates*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/ChannelsMERGED_WGamma/QuickChecks/
+
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/MUON_ZGamma/PrepareYields/*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/MUON_ZGamma/PrepareYields/
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/ELECTRON_ZGamma/PrepareYields/*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/ELECTRON_ZGamma/PrepareYields/
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/ELECTRON_ZGamma/CrossSection/*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/ELECTRON_ZGamma/CrossSection/
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/MUON_ZGamma/CrossSection/*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/MUON_ZGamma/CrossSection/
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/ChannelsMERGED_ZGamma/CrossSection/*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/ChannelsMERGED_ZGamma/CrossSection/
+
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/MUON_ZGamma/TemplateFits/*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/MUON_ZGamma/TemplateFits/
+
+#cp -r /home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk/figs_v8/ELECTRON_ZGamma/TemplateFits/*pdf /home/hep/eavdeeva2/ThesisTextWg/nuthesis/figs/figs_v11/ELECTRON_ZGamma/TemplateFits/
 
 d1=/home/hep/eavdeeva2/releases-GIT/cms_notes/notes/AN-15-055/trunk
 d2=examples
 
-cp $d1/Samples.tex $d2/AN_Samples.tex
-cp $d1/ObjectID.tex $d2/AN_ObjectID.tex
-cp $d1/BackgroundSubtraction.tex $d2/AN_BackgroundSubtraction.tex
-cp $d1/Unfolding.tex $d2/AN_Unfolding.tex
-cp $d1/AccXEff.tex $d2/AN_AccXEff.tex
-cp $d1/Systematics.tex $d2/AN_Systematics.tex
-cp $d1/CrossSection.tex $d2/AN_CrossSection.tex
+# THOSE ARE ALREADY MODIFIED HERE:
+#cp $d1/Samples.tex $d2/AN_Samples.tex
+#cp $d1/ObjectID.tex $d2/AN_ObjectID.tex
 
-cp $d1/Appendix_ZgFSRandISRplots.tex $d2/
-cp $d1/Appendix_CompareTemplatesPlots.tex $d2/
-cp $d1/Appendix_TemplateFitPlots.tex $d2/
-cp $d1/Appendix_TemplateFitPlotsMCclosure.tex $d2/
-cp $d1/Appendix_TemplateFitPlotsMCclosureWjestPlusWg.tex $d2/
-cp $d1/Appendix_Code.tex $d2/
-cp $d1/Appendix_ZgCheck.tex $d2/
-cp $d1/Appendix_TemplateFitPlots_ZGamma.tex $d2/
-cp $d1/Appendix_TemplateFitPlotsMCclosure.tex $d2/
-cp $d1/Appendix_TemplateFitPlotsMCclosureWjestPlusWg.tex $d2/
-cp $d1/Appendix_Code.tex $d2/
-cp $d1/Appendix_ZgCheck.tex $d2/
-cp $d1/Appendix_TemplateFitPlots_ZGamma.tex $d2/
-cp $d1/Appendix_TemplateFitPlotsMCclosure_ZGamma.tex $d2/
-cp $d1/Appendix_WjetsCheck.tex $d2/
-cp $d1/Appendix_MCclosureCheck.tex $d2/
-cp $d1/Appendix_EtogFitPlots.tex $d2/
-cp $d1/Appendix_EtogFitPlotsNoWMtCut.tex $d2/
-cp $d1/Appendix_EtogTables.tex $d2/
-cp $d1/Appendix_CorrelationMatrices.tex $d2/
-cp $d1/Appendix_Mpholep1DatavsMCPlots.tex $d2/
+#cp $d1/BackgroundSubtraction.tex $d2/AN_BackgroundSubtraction.tex
+#cp $d1/Unfolding.tex $d2/AN_Unfolding.tex
+#cp $d1/AccXEff.tex $d2/AN_AccXEff.tex
+#cp $d1/Systematics.tex $d2/AN_Systematics.tex
+#cp $d1/CrossSection.tex $d2/AN_CrossSection.tex
+
+#cp $d1/Appendix_ZgFSRandISRplots.tex $d2/
+#cp $d1/Appendix_CompareTemplatesPlots.tex $d2/
+#cp $d1/Appendix_TemplateFitPlots.tex $d2/
+#cp $d1/Appendix_TemplateFitPlotsMCclosure.tex $d2/
+#cp $d1/Appendix_TemplateFitPlotsMCclosureWjestPlusWg.tex $d2/
+#cp $d1/Appendix_Code.tex $d2/
+#cp $d1/Appendix_ZgCheck.tex $d2/
+#cp $d1/Appendix_TemplateFitPlots_ZGamma.tex $d2/
+#cp $d1/Appendix_TemplateFitPlotsMCclosure_ZGamma.tex $d2/
+#cp $d1/Appendix_WjetsCheck.tex $d2/
+#cp $d1/Appendix_MCclosureCheck.tex $d2/
+#cp $d1/Appendix_EtogFitPlots.tex $d2/
+#cp $d1/Appendix_EtogFitPlotsNoWMtCut.tex $d2/
+#cp $d1/Appendix_EtogTables.tex $d2/
+#cp $d1/Appendix_CorrelationMatrices.tex $d2/
+#cp $d1/Appendix_Mpholep1DatavsMCPlots.tex $d2/
 
 
 
